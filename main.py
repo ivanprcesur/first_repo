a7ddb61e9b136036f163908416bc72611fcd0c30
Iván Pérez Ruiz <ivan.s264140@cesurformacion.com>
2021-12-17 19:02:00 +0100

    he actualizado las lineas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
+    print("mi numero 1 linea de codigo")
     print("mi primera linea de codigo")
-    print("mi primera linea de codigo")
-    print("mi primera linea de codigo")
+    print("mi cuarta linea de codigo")
+
